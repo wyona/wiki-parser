b1198ef5e0159e16f79221915aa2d67ea71d63e2
david <david@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-10-26 02:08:05 +0000

    using dom for tree rotation

--- jspwiki-parser/src/java/org/wyona/jspwiki/WikiParser.java
+++ jspwiki-parser/src/java/org/wyona/jspwiki/WikiParser.java
@@ -1,17 +1,25 @@
 package org.wyona.jspwiki;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Properties;
+import java.util.Vector;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.log4j.Category;
+import org.jdom.Content;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
 
 import com.ecyrd.jspwiki.WikiContext;
 import com.ecyrd.jspwiki.WikiEngine;
@@ -25,6 +33,7 @@ public class WikiParser extends org.wyona.wikiparser.WikiParser {
     
     private InputStream inputStream = null;
     private Properties properties = null;
+    private Element bodyElement = null; 
     
     public WikiParser(InputStream is) {
         init(is);
@@ -74,14 +83,90 @@ public class WikiParser extends org.wyona.wikiparser.WikiParser {
             createdHtml.append("<html><body>");
             createdHtml.append(engine.textToHTML(context, stringBuffer.toString()));
             createdHtml.append("</body></html>");
-            log.debug(createdHtml.toString());
+            
+            ByteArrayInputStream inStream = new ByteArrayInputStream(createdHtml.toString().getBytes());
+            SAXBuilder builder = new SAXBuilder();
+            Document doc = builder.build(inStream);
+            Element root = doc.getRootElement();
+            bodyElement = root.getChild("body");
+            List listRootElements = bodyElement.getChildren();
+            for(int i=0; i<listRootElements.size(); i++) {
+                Element element  = (Element)(listRootElements.get(i));
+                if(element.getName().equalsIgnoreCase("ul") || element.getName().equalsIgnoreCase("ol")) {
+                    treeWalker(element, 1);
+                }
+            }
+            
+            XMLOutputter outputter = new XMLOutputter();
+            
+            String modifiedHtml = outputter.outputString(doc);
+            System.out.println("###" + modifiedHtml + "\n\n");
             Html2WikiXmlTransformer html2WikiXml = new Html2WikiXmlTransformer();
             SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            saxParser.parse(new java.io.ByteArrayInputStream(createdHtml.toString().getBytes()), html2WikiXml);
+            //saxParser.parse(new java.io.ByteArrayInputStream(createdHtml.toString().getBytes()), html2WikiXml);
+            saxParser.parse(new java.io.ByteArrayInputStream(modifiedHtml.getBytes()), html2WikiXml);
+            
             setResultAsInputStream(html2WikiXml.getInputStream());
         } catch(Exception e) {
-            log.error(e);
+            e.printStackTrace();
         }
+    }
+    
+    public void treeWalker(Content node, int counter) {
+        if(node instanceof Element) {
+            Element element = (Element) node;
+            if(!("true").equals(element.getAttributeValue("moved"))) {
+                if(element.getName().equals("li")) {
+                    element.setAttribute("depth", "" + counter);
+                    counter++;
+                }    
+                List listChildren = element.getChildren();
+                for(int i=0; i<listChildren.size(); i++) {
+                    treeWalker((Element) listChildren.get(i), counter);
+                }
+                if(element.getName().equals("ul")) {
+                    moveElement(element, "ul");
+                }
+                if(element.getName().equals("ol")) {
+                    moveElement(element, "ol");
+                }
+                element.setAttribute("moved", "true");
+            }
+        }
+    }
+    
+    public void moveElement(Element element, String name) {
+        int position = 0;
+        Vector keepThisKids = new Vector();
+        Element grandParentElement = element.getParentElement().getParentElement();
+        if(grandParentElement.getName().equals(name)) {
+            Element temp = element.getParentElement();
+            List movingChildren = element.getChildren();
+            for(int i=0; i<movingChildren.size(); i++) {
+                keepThisKids.add(movingChildren.get(i));
+                ((Element)(movingChildren.get(i))).detach();
+            }
+            element.detach();
+            position = grandParentElement.indexOf(temp) + 1;
+            grandParentElement.addContent(position, keepThisKids);
+        } else 
+        if(name.equals("ol") && grandParentElement.getName().equals("ul") || name.equals("ul") && grandParentElement.getName().equals("ol")) {
+            Element subRoot = getActualRootsChild(element);
+            List movingChildren = element.getChildren();
+            for(int i=0; i<movingChildren.size(); i++) {
+                keepThisKids.add(movingChildren.get(i));
+                ((Element)(movingChildren.get(i))).detach();
+            }
+            element.detach();
+            position = bodyElement.indexOf(subRoot) + 1;
+            bodyElement.addContent(position, keepThisKids);
+        }
+        //TODO handle this bug DONT know where to attach this 
+    }
+    
+    public Element getActualRootsChild(Element element) {
+        if(element.getParentElement().equals(bodyElement)) return element;
+        else return getActualRootsChild(element.getParentElement());
     }
     
     /**
